bf74feda2e9c1d392b977af909320e61d4551ab6
Your Name <you@example.com>
2023-10-16 05:22:00 +0000

    Item Name auto creation

--- customization/item.py
+++ customization/item.py
@@ -1,3 +1,6 @@
+import frappe
+
 def customize_name(doc, action):
- 	if doc.item_group == "POWER TOOLS":
- 		doc.item_name = f'{doc.brand} {doc.item_name} {doc.item_model}'
+	if doc.item_group == "POWER TOOLS" and doc.is_power_tools == 0 :
+		doc.is_power_tools = 1
+		doc.item_name = f'{doc.brand} {doc.item_name} {doc.item_model}' 
